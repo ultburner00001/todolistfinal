31f22f22977a9ce20f843d40b62e34052d917d43
mehulswami2006 <mehulswami2006@gmail.com>
2025-10-30 00:24:28 +0530

    Refactor Todo model and update schema fields
    Refactor Todo model to use ES6 import/export syntax and update schema fields.

--- backend/models/Todo.js
+++ backend/models/Todo.js
@@ -1,23 +1,9 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-const todoSchema = new mongoose.Schema(
-  {
-    text: { type: String, required: true },
-    completed: { type: Boolean, default: false },
-    category: {
-      type: String,
-      enum: [
-        "important_urgent",
-        "important_not_urgent",
-        "not_important_urgent",
-        "not_important_not_urgent",
-      ],
-      default: "not_important_not_urgent",
-    },
-    dueDate: { type: Date, default: null },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  },
-  { timestamps: true }
-);
+const todoSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  completed: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now },
+});
 
-module.exports = mongoose.model("Todo", todoSchema);
+export default mongoose.model("Todo", todoSchema);
